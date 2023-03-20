50fc6ce00530f492f7e705dfe24774419b852fc6
khot29 <huzaifakhot2911@gmail.com>
2023-03-20 10:24:25 +0530

    20th March 10:24am

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './layouts/header/header.component';
+import { CategoryNavbarComponent } from './layouts/category-navbar/category-navbar.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    CategoryNavbarComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
